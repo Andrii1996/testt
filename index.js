7bb81de7f090e271f59611e59478962b0a35a515
Andrii1996 <a.medzyak@gmail.com>
2021-03-01 15:53:24 +0200

    update date

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ import uuidv4 from "./src/helper/uuid";
 
 import express from 'express';
 import bodyParser from 'body-parser';
-import path from 'path';
+// import path from 'path';
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static("public"));
 
 const PORT = process.env.PORT || 3001;
 
